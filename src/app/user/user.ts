7adc9e579cdb18b3f2e8efee37f078b9111c3474
Stephan <stephan-steiner-dev@protonmail.com>
2026-04-12 19:34:30 +0200

    showUsers

--- src/app/user/user.ts
+++ src/app/user/user.ts
@@ -1,23 +1,44 @@
 import { Component, inject } from '@angular/core';
-import {MatButtonModule} from '@angular/material/button';
-import { MatIconModule} from '@angular/material/icon';
-import {MatTooltipModule} from '@angular/material/tooltip';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatTooltipModule } from '@angular/material/tooltip';
 import { MatDialogModule, MatDialog } from '@angular/material/dialog';
 import { DialogAddUser } from '../dialog-add-user/dialog-add-user';
 import { AddUser } from '../../models/user.class';
-import {MatCardModule} from '@angular/material/card';
+import { MatCardModule } from '@angular/material/card';
+import { Firestore, collection, collectionData } from '@angular/fire/firestore';
+import { CommonModule } from '@angular/common';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-user',
   standalone: true,
-  imports: [MatButtonModule, MatIconModule, MatTooltipModule, MatDialogModule, MatCardModule],
+  imports: [
+    MatButtonModule,
+    MatIconModule,
+    MatTooltipModule,
+    MatDialogModule,
+    MatCardModule,
+    CommonModule,
+    RouterLink
+  ],
   templateUrl: './user.html',
   styleUrl: './user.scss',
 })
 export class User {
-  public dialog = inject(MatDialog)
+  public dialog = inject(MatDialog);
+  public firestore = inject(Firestore);
+  public allUsers: any[] = [];
 
   openDialog() {
-    this.dialog.open(DialogAddUser)
+    this.dialog.open(DialogAddUser);
+  }
+
+  ngOnInit(): void {
+    const usersRef = collection(this.firestore, 'users');
+    collectionData(usersRef, {idField: 'firebaseId'}).subscribe((changes) => {
+      this.allUsers = changes as User[];
+      console.log(this.allUsers)
+    });
   }
 }
--- src/models/user.class.ts
+++ src/models/user.class.ts
@@ -1,15 +1,19 @@
 export class AddUser {
+  firebaseId: string;
   firstName: string;
   lastName: string;
   birthDate: number;
+  email: string;
   address: string;
   zipCode: number;
   city: string;
 
   constructor(obj?: any) {
+    this.firebaseId = obj?.firebaseId || '';
     this.firstName = obj?.firstName || '';
     this.lastName = obj?.lastName || '';
     this.birthDate = obj?.birthDate || 0;
+    this.email = obj?.email || '';
     this.address = obj?.address || '';
     this.zipCode = obj?.zipCode || 0;
     this.city = obj?.city || '';
@@ -17,9 +21,11 @@ export class AddUser {
 
   toJSON() {
     return {
+      id: this.firebaseId,
       firstName: this.firstName,
       lastName: this.lastName,
       birthDate: this.birthDate,
+      email: this.email,
       address: this.address,
       zipCode: this.zipCode,
       city: this.city,
