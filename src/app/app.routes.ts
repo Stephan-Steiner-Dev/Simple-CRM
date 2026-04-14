8f2ceada051bda7d2cf488680a1efd2205d3f434
Stephan <stephan-steiner-dev@protonmail.com>
2026-04-14 20:13:55 +0200

    user-details

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,10 +2,12 @@ import { Routes } from '@angular/router';
 import { Dashboard } from './dashboard/dashboard';
 import { User } from './user/user';
 import { DialogAddUser } from './dialog-add-user/dialog-add-user';
+import { UserDetail } from './user-detail/user-detail';
 
 export const routes: Routes = [
   { path: '', component: Dashboard },
   { path: 'dashboard', component: Dashboard },
   { path: 'user', component: User },
   { path: 'user', component: DialogAddUser },
+  { path: 'user/:id', component: UserDetail },
 ];
---
+++ src/app/user-detail/user-detail.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { MatCardModule } from '@angular/material/card';
+
+@Component({
+  selector: 'app-user-detail',
+  imports: [MatCardModule],
+  templateUrl: './user-detail.html',
+  styleUrl: './user-detail.scss',
+})
+export class UserDetail {
+
+}
