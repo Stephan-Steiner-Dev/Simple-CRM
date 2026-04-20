877b173f445a2bcfcf7f4eb9ea6454d697517db2
Stephan <stephan-steiner-dev@protonmail.com>
2026-04-20 20:35:02 +0200

    user-details

--- src/app/user-detail/user-detail.ts
+++ src/app/user-detail/user-detail.ts
@@ -1,12 +1,37 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, inject } from '@angular/core';
+import { Firestore, doc, docData } from '@angular/fire/firestore';
 import { MatCardModule } from '@angular/material/card';
+import { ActivatedRoute } from '@angular/router';
+import { Observable } from 'rxjs';
+import { MatIcon } from '@angular/material/icon';
+
 
 @Component({
   selector: 'app-user-detail',
-  imports: [MatCardModule],
+  imports: [MatCardModule, MatIcon, CommonModule],
   templateUrl: './user-detail.html',
   styleUrl: './user-detail.scss',
 })
-export class UserDetail {
+export class UserDetail{
+  firestore = inject(Firestore);
+  route = inject(ActivatedRoute);
+  userId: string | null = null;
+  user$!: Observable<any>;
 
+  ngOnInit() {
+    this.userId = this.route.snapshot.paramMap.get('id');
+
+    if (this.userId) {
+      this.user$ = this.getUser(this.userId);
+      console.log(this.user$)
+    }
+  }
+
+  getUser(userId: string): Observable<any> {
+    const userDocRef = doc(this.firestore, 'users', userId);
+    return docData(userDocRef, { idField: 'id' }) as Observable<any>;
+  }
+
+  openAddressDialog() {}
 }
